b35ce5188e93ad93a342b0d3c0602be11bce5761
Fakhr Murshid <murshid2@gmail.com>
2015-03-03 17:01:28 -0500

    First Commit with index page

---
+++ MCWSWebsit/WebContent/images/slide1_files/ca-pub-0385187085459372.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-0385187085459372'] = {"sra_enabled":true};
---
+++ MCWSWebsit/WebContent/images/slide1_files/in.php
@@ -0,0 +1,4 @@
+// static8
+if( window._genericStats ) _genericStats.set_cookie('_jsuid', '769266765');
+// exit trax0r
+var o_O
